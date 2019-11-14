d684f83a19fe37e6be0c1a8233bd33adbd9222dd
Travis <travisstew214@gmail.com>
2019-11-13 23:46:56 -0500

    package json

--- config/connection.js
+++ config/connection.js
@@ -2,19 +2,37 @@
 
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var mysqlConfig = {
   host: 'us-cdbr-iron-east-05.cleardb.net',
   user: "b0edb8a497cc4a",
   password: 'da3c3646',
   database: 'heroku_fedf47f604bb52d'
 
-});
+};
+var connection = mysql.createConnection(mysqlConfig);
+handleDisconnect(mysqlClient);
 
-connection.connect(function(err, res){
-  if(err) throw err.code;
-  console.log('connected to burgers_db schema');
+
+// connection.connect(function(err, res){
+//   if(err) throw err.code;
+//   console.log('connected to burgers_db schema');
   
-});
+// });
 
+function handleDisconnect(client) {
+  client.on('error', function (error) {
+    if (!error.fatal) return;
+    if (error.code !== 'PROTOCOL_CONNECTION_LOST') throw err;
+
+    console.error('> Re-connecting lost MySQL connection: ' + error.stack);
+
+    // NOTE: This assignment is to a variable from an outer scope; this is extremely important
+    // If this said `client =` it wouldn't do what you want. The assignment here is implicitly changed
+    // to `global.mysqlClient =` in node.
+    mysqlClient = mysql.createConnection(client.config);
+    handleDisconnect(mysqlClient);
+    mysqlClient.connect();
+  });
+};
 module.exports = connection;
 
